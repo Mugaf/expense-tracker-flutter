e813fae3ec2c9f6cc1ec78b34ac3221304ddc94c
Muhlifain Firgana <muhlifain@mhcasiagroup.com>
2023-11-13 20:28:56 +0800

    add theme style for whole app

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
-import 'package:expense_tracker/widgets/expenses.dart';
+import 'package:expense_tracker/widgets/main_widget.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    theme: ThemeData(useMaterial3: true),
-    home: const Expenses(),
-  ));
+  runApp(const MainWidget());
 }
