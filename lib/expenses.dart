6ede5f64dbd8534d9ca676c606826e24710ab401
Muhlifain Firgana <muhlifain@mhcasiagroup.com>
2023-11-13 10:31:16 +0800

    rename expense file to expenses for widget

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -9,6 +10,20 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Flutter Course',
+      amount: 10.0,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'Bus',
+      amount: 2.0,
+      date: DateTime.now(),
+      category: Category.travel,
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
