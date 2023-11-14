b798c2adf521750129f66fbeeb91e12c6d800caf
Muhlifain Firgana <muhlifain@mhcasiagroup.com>
2023-11-14 10:52:29 +0800

    make the main screen ui responsive

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,6 @@ import 'package:expense_tracker/widgets/main_widget.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MainWidget());
 }
--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -66,6 +66,8 @@ class _ExpensesState extends State<Expenses> {
 
   @override
   Widget build(BuildContext context) {
+    final widgetWidth = MediaQuery.of(context).size.width;
+
     Widget mainContent = const Center(
       child: Text('No Expenses yet. please add some ...'),
     );
@@ -86,14 +88,23 @@ class _ExpensesState extends State<Expenses> {
           )
         ],
       ),
-      body: Column(
-        children: [
-          Chart(expenses: _registeredExpenses),
-          Expanded(
-            child: mainContent,
-          ),
-        ],
-      ),
+      body: widgetWidth < 600
+          ? Column(
+              children: [
+                Chart(expenses: _registeredExpenses),
+                Expanded(
+                  child: mainContent,
+                ),
+              ],
+            )
+          : Row(
+              children: [
+                Expanded(child: Chart(expenses: _registeredExpenses)),
+                Expanded(
+                  child: mainContent,
+                ),
+              ],
+            ),
     );
   }
 }
